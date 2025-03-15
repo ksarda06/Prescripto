0289380ae3b2d6800264e90a17a042f9a540e73d
ksarda06 <143283847+ksarda06@users.noreply.github.com>
2025-03-15 18:18:34 +0530

    Integrated payment gateway

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken'
 import cloudinary from 'cloudinary'
 import appointmentModel from '../models/appointmentModel.js'
 import doctorModel from '../models/doctorsModel.js'
-//import razorpay from 'razorpay'
+import razorpay from 'razorpay'
 const registerUser=async(req,res)=>{
     try{
         const {name,email,password}=req.body
@@ -159,12 +159,47 @@ const cancelAppointment= async(req,res) =>{
     }
 
 }
-/*const razorpayInstance=new razorpay({
-    key_id:'',
-    key_secret:''
+const razorpayInstance=new razorpay({
+    key_id:process.env.RAZORPAY_KEY_ID,
+    key_secret:process.env.RAZORPAY_KEY_SECRET
 })
-const paymentRazorpay=async(req,rees)=>{
-    
-}*/
+const paymentRazorpay=async(req,res)=>{
+   try{ 
+    const {appointmentId}=req.body
+    const appointmentData=await appointmentModel.findById(appointmentId)
+    if(!appointmentData || appointmentData.cancelled){
+        return res.json({success:false,message:"Appointment Cancelled or not found"})
 
-export {registerUser,loginUser,getProfile,updateProfile,bookAppointment,listAppointment,cancelAppointment}
+    }
+    const options={
+        amount:appointmentData.amount*100,
+        currency:process.env.CURRENCY, 
+        receipt:appointmentId
+    }
+    const order=await razorpayInstance.orders.create(options)
+    res.json({success:true,order})
+  }
+  catch(err){
+    console.log(err)
+    res.json({success:false,message:err.message})
+  }
+}
+
+const verifyRazorpay=async(req,res)=>{
+    try{
+        const {razorpay_order_id}=req.body
+        const orderInfo=await razorpayInstance.orders.fetch(razorpay_order_id)
+        if(orderInfo.status==='paid'){
+           await appointmentModel.findByIdAndUpdate(orderInfo.receipt,{payment:true})
+           res.json({success:true,message:"Payment Successfull"})
+        }
+        else{
+            res.json({success:false,message:"Payment Failed"})
+        }
+    }
+    catch(err){
+       console.log(err)
+       res.json({success:false,message:err.message})
+    }
+}
+export {registerUser,loginUser,getProfile,updateProfile,bookAppointment,listAppointment,cancelAppointment,paymentRazorpay,verifyRazorpay}

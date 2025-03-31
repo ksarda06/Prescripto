7890099e69ceba8f559b28725f557afe51a4a122
ksarda06 <143283847+ksarda06@users.noreply.github.com>
2025-03-31 18:27:42 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -11,7 +11,7 @@ const app=express()
 const port=process.env.PORT || 4000
 connectDB()
 connectCloudinary()
-const allowedOrigins=['http://localhost:5173']
+const allowedOrigins=['https://prescripto-a5to.onrender.com']
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({origin:allowedOrigins,credentials:true}))

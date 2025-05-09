fd047ca92f4e542c0a39b24d34c922469fcd437d
ksarda06 <143283847+ksarda06@users.noreply.github.com>
2025-05-09 19:39:32 +0530

    Update authUser.js

--- Backend/middleware/authUser.js
+++ Backend/middleware/authUser.js
@@ -3,7 +3,7 @@ const authUser=async(req,res,next)=>{
      try{
        const {token}=req.cookies
         if(!token){
-            return res.json({sucess:false,message:'Not Authorized login again'})
+            return res.json({sucess:false,message:'Not Authorized login again1'})
         }
         const token_decode=jwt.verify(token,process.env.JWT_SECRET)
         req.body.userId=token_decode.id

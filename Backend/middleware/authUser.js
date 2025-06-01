e54f3e13e62a2da53360dcf3619287431bdb52e1
ksarda06 <143283847+ksarda06@users.noreply.github.com>
2025-06-01 16:14:24 +0530

    Update authUser.js

--- Backend/middleware/authUser.js
+++ Backend/middleware/authUser.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
 const authUser=async(req,res,next)=>{
      try{
-       const {token}=req.cookies || localStorage.getItem(token)
+       const {token}=req.cookies || localStorage.getItem(token) || req.headers
         if(!token){
             return res.json({sucess:false,message:'Not Authorized login again1'})
         }

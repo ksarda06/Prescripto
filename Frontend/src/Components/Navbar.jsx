99471a381c7805c70b377d4025efb830ec3c6572
ksarda06 <143283847+ksarda06@users.noreply.github.com>
2025-06-03 14:32:03 +0530

    Update Navbar.jsx

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -62,7 +62,7 @@ const Navbar = () => {
       </ul>
       <div className='flex item-center gap-4'>
         {
-            token && userData ? <div className='flex items-center gap-2 cursor-pointer group relative'>
+            token ? <div className='flex items-center gap-2 cursor-pointer group relative'>
                 <img className='w-8 rounded-full'src={userData.image}></img>
                 <img className='w-2.5'src={assets.dropdown_icon} />                
             <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
